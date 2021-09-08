a7a6210b5f9298d9f364d2e81c8ead901924c3ae
Enno Boland <g@s01.de>
2021-09-08 15:18:47 +0200

    do a directory iteration on fuzzing

--- fuzzer/simple.c
+++ fuzzer/simple.c
@@ -5,11 +5,27 @@
  * Distributed under terms of the MIT license.
  */
 
+#include "../src/context/directory_context.h"
+#include "../src/context/inode_context.h"
 #include "../src/squash.h"
 #include <stdint.h>
 
-int LLVMFuzzerTestOneInput(char *data, size_t size) {
-	struct Squash squash = { 0 };
+int
+LLVMFuzzerTestOneInput(char *data, size_t size) {
+	struct Squash squash = {0};
+	struct SquashInodeContext inode = {0};
+	struct SquashDirectoryContext dir = {0};
+	struct SquashDirectoryIterator iter = {0};
 	squash_init(&squash, (uint8_t *)data, size, SQUASH_DTOR_NONE);
-	return 0;  // Non-zero return values are reserved for future use.
+
+	squash_inode_load_ref(&inode, &squash,
+			squash_superblock_root_inode_ref(squash.superblock));
+
+	squash_directory_init(&dir, &squash, &inode);
+	squash_directory_iterator_init(&iter, &dir);
+	while (squash_directory_iterator_next(&iter)) {
+	}
+	squash_directory_iterator_clean(&iter);
+
+	return 0; // Non-zero return values are reserved for future use.
 }
