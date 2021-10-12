35cb53bb32fcdc112a744e71d5b9dfa41fe6ce8e
Enno Boland <g@s01.de>
2021-10-12 14:26:54 +0200

    test/list: add test for listing files

--- Makefile
+++ Makefile
@@ -71,7 +71,8 @@ BIN = \
 	bin/squashfs-fuse \
 
 TST = \
-	test/utils/lru_hashmap.c
+	test/utils/lru_hashmap.c \
+	test/list.c \
 
 TST_BIN = $(TST:.c=-test)
 
@@ -145,7 +146,7 @@ bench/%-bench: bench/%.c test/test.h $(OBJ)
 	@echo CCBENCH $@
 	@$(CC) $(CFLAGS) $(LDFLAGS) $(BCH_CFLAGS) $(OBJ) $< -o $@
 
-test/%-test: test/%.c test/test.h $(SRC)
+test/%-test: test/%.c test/test.h $(SRC) test/squash_image.h
 	@echo CCTEST $@
 	@$(CC) $(CFLAGS) $(LDFLAGS) $(TST_CFLAGS) $(SRC) $< -o $@
 
@@ -182,10 +183,21 @@ coverage: check
 	@mkdir cov
 	@gcovr -r . --html --html-details -o cov/index.html
 
+test/squash_image.h:
+	mkdir -p $@_dir
+	echo a > $@_dir/b
+	echo b > $@_dir/b
+	rm -f $@.squashfs
+	mksquashfs $@_dir $@.squashfs -noI -noId -noD -noF -noX -nopad
+	printf "%s\n" "#include <stdint.h>" "static const uint8_t squash_image[] = {" > $@
+	od  $@.squashfs -t u1 -v -A n | tr ' ' '\n' | grep -v '^$$' | paste -sd, >> $@
+	echo "};" >> $@
+	rm -r $@_dir $@.squashfs
+
 clean:
 	@echo cleaning...
 	@rm -rf doc cov
-	@rm -f *.gcnp *.gcda
+	@rm -f *.gcnp *.gcda test/squash_image.h
 	@rm -f $(TST_BIN) $(BCH_BIN) $(FZZ_BIN) $(OBJ) $(BIN) libsquashfs.so* libsquashfs.a
 
 .PHONY: check all clean speed coverage fuzz
---
+++ test/list.c
@@ -0,0 +1,99 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (c) 2021, Enno Boland <g@s01.de>                                 *
+ *                                                                            *
+ * Redistribution and use in source and binary forms, with or without         *
+ * modification, are permitted provided that the following conditions are     *
+ * met:                                                                       *
+ *                                                                            *
+ * * Redistributions of source code must retain the above copyright notice,   *
+ *   this list of conditions and the following disclaimer.                    *
+ * * Redistributions in binary form must reproduce the above copyright        *
+ *   notice, this list of conditions and the following disclaimer in the      *
+ *   documentation and/or other materials provided with the distribution.     *
+ *                                                                            *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS    *
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,  *
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR     *
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR          *
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,      *
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,        *
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR         *
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF     *
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING       *
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS         *
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.               *
+ *                                                                            *
+ ******************************************************************************/
+
+/**
+ * @author      : Enno Boland (mail@eboland.de)
+ * @file        : list
+ * @created     : Monday Oct 11, 2021 21:43:12 CEST
+ */
+
+#include "../src/context/directory_context.h"
+#include "../src/context/inode_context.h"
+#include "../src/context/superblock_context.h"
+#include "../src/data/superblock.h"
+#include "../src/error.h"
+#include "common.h"
+#include "squash_image.h"
+#include "test.h"
+
+static void
+squash_ls() {
+	int rv;
+	char *name;
+	struct SquashSuperblockContext superblock = {0};
+	struct SquashInodeContext inode = {0};
+	struct SquashDirectoryContext dir = {0};
+	struct SquashDirectoryIterator iter = {0};
+	rv = squash_superblock_init(
+			&superblock, squash_image, sizeof(squash_image));
+	assert(rv == 0);
+
+	rv = squash_inode_load(&inode, &superblock,
+			squash_data_superblock_root_inode_ref(superblock.superblock));
+	squash_perror(rv, NULL);
+	assert(rv == 0);
+
+	rv = squash_directory_init(&dir, &superblock, &inode);
+	assert(rv == 0);
+
+	rv = squash_directory_iterator_init(&iter, &dir);
+	assert(rv == 0);
+
+	rv = squash_directory_iterator_next(&iter);
+	assert(rv == 0);
+	rv = squash_directory_iterator_name_dup(&iter, &name);
+	assert(rv == 0);
+	assert(strcmp("a", name) == 0);
+	free(name);
+
+	rv = squash_directory_iterator_next(&iter);
+	assert(rv == 0);
+	rv = squash_directory_iterator_name_dup(&iter, &name);
+	assert(rv == 0);
+	assert(strcmp("b", name) == 0);
+	free(name);
+
+	rv = squash_directory_iterator_next(&iter);
+	assert(rv != 0);
+
+	rv = squash_directory_iterator_cleanup(&iter);
+	assert(rv == 0);
+
+	rv = squash_directory_cleanup(&dir);
+	assert(rv == 0);
+
+	rv = squash_inode_cleanup(&inode);
+	assert(rv == 0);
+
+	rv = squash_superblock_cleanup(&superblock);
+	assert(rv == 0);
+}
+
+DEFINE
+TEST(squash_ls);
+DEFINE_END
