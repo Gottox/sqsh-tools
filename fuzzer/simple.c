a78225a1f428e66f900b722053b8f1a3dc064af0
Enno Boland <g@s01.de>
2021-10-17 16:00:33 +0200

    fuzzer: try to read file while fuzzing

--- fuzzer/simple.c
+++ fuzzer/simple.c
@@ -6,11 +6,31 @@
  */
 
 #include "../src/context/directory_context.h"
+#include "../src/context/file_context.h"
 #include "../src/context/inode_context.h"
 #include "../src/context/superblock_context.h"
 #include "../src/squash.h"
 #include <stdint.h>
 
+static int
+read_file(struct SquashInodeContext *inode) {
+	struct SquashFileContext file = {0};
+	int rv;
+	rv = squash_file_init(&file, inode);
+	if (rv < 0) {
+		goto out;
+	}
+
+	rv = squash_file_read(&file, squash_inode_file_size(inode));
+	if (rv < 0) {
+		goto out;
+	}
+
+out:
+	squash_file_cleanup(&file);
+	// noop
+	return 0;
+}
 int
 LLVMFuzzerTestOneInput(char *data, size_t size) {
 	int rv = 0;
@@ -23,7 +43,8 @@ LLVMFuzzerTestOneInput(char *data, size_t size) {
 		goto out;
 	}
 
-	rv = squash_inode_load(&inode, &squash.superblock,
+	rv = squash_inode_load(
+			&inode, &squash.superblock,
 			squash_superblock_inode_root_ref(&squash.superblock));
 	if (rv < 0) {
 		goto out;
@@ -38,13 +59,15 @@ LLVMFuzzerTestOneInput(char *data, size_t size) {
 		goto out;
 	}
 	while (squash_directory_iterator_next(&iter)) {
-		// noop
+		if (read_file(&inode) < 0)
+			break;
 	}
 
 out:
 
 	squash_directory_iterator_cleanup(&iter);
 	squash_directory_cleanup(&dir);
+	squash_inode_cleanup(&inode);
 	squash_inode_cleanup(&inode);
 
 	return rv; // Non-zero return values are reserved for future use.
