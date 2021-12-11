1dba835d86dfa7c642cbab9c966af3c486aabaaa
Enno Boland <g@s01.de>
2021-12-12 00:17:49 +0100

    move to meson

---
+++ src/compression/lzo2.c
@@ -0,0 +1,68 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (c) 2021, Enno Boland <g@s01.de>                                 *
+ *                                                                            *
+ * Redistribution and use in source and binary forms, with or without         *
+ * modification, are permitted provided that the following conditions are     *
+ * met:                                                                       *
+ *                                                                            *
+ * * Redistributions of source code must retain the above copyright notice,   *
+ *   this list of conditions and the following disclaimer.                    *
+ * * Redistributions in binary form must reproduce the above copyright        *
+ *   notice, this list of conditions and the following disclaimer in the      *
+ *   documentation and/or other materials provided with the distribution.     *
+ *                                                                            *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS    *
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,  *
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR     *
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR          *
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,      *
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,        *
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR         *
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF     *
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING       *
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS         *
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.               *
+ *                                                                            *
+ ******************************************************************************/
+
+/**
+ * @author      : Enno Boland (mail@eboland.de)
+ * @file        : lzo
+ * @created     : Sunday Sep 05, 2021 11:09:51 CEST
+ */
+
+#include <lzo/lzo1x.h>
+#include <lzo/lzoconf.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <zconf.h>
+
+#include "../data/compression_options.h"
+#include "../error.h"
+#include "compression.h"
+
+static int
+hsqs_lzo2_extract(
+		const union HsqsCompressionOptions *options, size_t options_size,
+		uint8_t *target, size_t *target_size, const uint8_t *compressed,
+		const size_t compressed_size) {
+	if (options != NULL &&
+		options_size != HSQS_SIZEOF_COMPRESSION_OPTIONS_LZO) {
+		return -HSQS_ERROR_COMPRESSION_DECOMPRESS;
+	}
+
+	int rv = lzo1x_decompress_safe(
+			compressed, compressed_size, target, target_size, NULL);
+
+	if (rv != LZO_E_OK) {
+		return -HSQS_ERROR_COMPRESSION_DECOMPRESS;
+	}
+	return 0;
+}
+
+const struct HsqsCompressionImplementation hsqs_compression_lzo = {
+		.extract = hsqs_lzo2_extract,
+};
---
+++ utils/create_squashfs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+in=$1
+out=$2
+tmp=$3
+
+mkdir "$tmp"
+echo a > "$tmp/a"
+seq 1 1050000 | tr -cd "\n" | tr '\n' b > "$tmp/b"
+$SETFATTR -n user.foo -v 1234567891234567891234567890001234567890 "$tmp/a"
+$SETFATTR -n user.bar -v 1234567891234567891234567890001234567890 "$tmp/b"
+$MKSQUASHFS "$tmp" "$out" \
+	-noI -noId -noD -noF -noX \
+	-nopad \
+	-force-uid 2020 \
+	-force-gid 202020 \
+	-Xcompression-level 1 \
+	-always-use-fragments \
+	-quiet
