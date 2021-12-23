c2087969406705fe642c54b5f8fd78ebd5715742
Enno Boland <g@s01.de>
2021-12-23 18:45:20 +0100

    add trace functionality

---
+++ src/utils/trace.c
@@ -0,0 +1,48 @@
+/**
+ * @author      : Enno Boland (mail@eboland.de)
+ * @file        : trace
+ * @created     : Thursday Dec 23, 2021 15:26:56 CET
+ */
+
+#include <execinfo.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define STACK_POS 4
+
+static void __attribute__((no_instrument_function)) print_func(char *action) {
+	void *array[100];
+	int size;
+	char **strings;
+
+	size = backtrace(array, 100);
+	strings = backtrace_symbols(array, size);
+
+	int i;
+	for (i = 0; i < size; i++) {
+		if (strstr(strings[i], "__cyg_profile_func")) {
+			i++;
+			break;
+		}
+	}
+	if (i < size) {
+		printf("%s %s\n", action, strings[i]);
+	}
+	free(strings);
+}
+
+void __attribute__((no_instrument_function))
+__cyg_profile_func_enter(void *func, void *caller) {
+	(void)func;
+	(void)caller;
+	print_func("=>");
+}
+
+void __attribute__((no_instrument_function))
+__cyg_profile_func_exit(void *func, void *caller) {
+	(void)func;
+	(void)caller;
+	print_func("<=");
+}
