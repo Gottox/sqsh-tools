b240241c28ee7f4b6eff69ba0bb1571b291e2081
Enno Boland <g@s01.de>
2023-02-26 21:55:14 +0100

    test: add compression tests

---
+++ test/compression/compression.c
@@ -0,0 +1,154 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2023, Enno Boland
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @author       Enno Boland (mail@eboland.de)
+ * @file         buffer.c
+ */
+
+#include "../common.h"
+#include "../test.h"
+
+#include <sqsh_compression_private.h>
+#include <stdint.h>
+
+int sqsh_extract_lzma(
+		uint8_t *target, size_t *target_size, const uint8_t *compressed,
+		const size_t compressed_size);
+int sqsh_extract_xz(
+		uint8_t *target, size_t *target_size, const uint8_t *compressed,
+		const size_t compressed_size);
+int sqsh_extract_lz4(
+		uint8_t *target, size_t *target_size, const uint8_t *compressed,
+		const size_t compressed_size);
+int sqsh_extract_lzo2(
+		uint8_t *target, size_t *target_size, const uint8_t *compressed,
+		const size_t compressed_size);
+int sqsh_extract_zlib(
+		uint8_t *target, size_t *target_size, const uint8_t *compressed,
+		const size_t compressed_size);
+int sqsh_extract_zstd(
+		uint8_t *target, size_t *target_size, const uint8_t *compressed,
+		const size_t compressed_size);
+
+static void
+decompress_lzma(void) {
+	uint8_t input[] = {0x5d, 0x00, 0x00, 0x80, 0x00, 0xff, 0xff, 0xff, 0xff,
+					   0xff, 0xff, 0xff, 0xff, 0x00, 0x30, 0x98, 0x88, 0x98,
+					   0x46, 0x7e, 0x1e, 0xb2, 0xff, 0xfa, 0x1c, 0x80, 0x00};
+	size_t target_size = 4;
+	uint8_t output[4];
+
+	int rv = sqsh_extract_lzma(output, &target_size, input, sizeof(input));
+	assert(rv >= 0);
+	assert(target_size == 4);
+	assert(memcmp(output, "abcd", 4) == 0);
+}
+
+static void
+decompress_xz(void) {
+	uint8_t input[] = {0xfd, 0x37, 0x7a, 0x58, 0x5a, 0x00, 0x00, 0x04, 0xe6,
+					   0xd6, 0xb4, 0x46, 0x02, 0x00, 0x21, 0x01, 0x16, 0x00,
+					   0x00, 0x00, 0x74, 0x2f, 0xe5, 0xa3, 0x01, 0x00, 0x03,
+					   0x61, 0x62, 0x63, 0x64, 0x00, 0xba, 0x60, 0x59, 0x6e,
+					   0x59, 0x28, 0x9d, 0x3c, 0x00, 0x01, 0x1c, 0x04, 0x6f,
+					   0x2c, 0x9c, 0xc1, 0x1f, 0xb6, 0xf3, 0x7d, 0x01, 0x00,
+					   0x00, 0x00, 0x00, 0x04, 0x59, 0x5a};
+	size_t target_size = 4;
+	uint8_t output[4];
+
+	int rv = sqsh_extract_xz(output, &target_size, input, sizeof(input));
+	assert(rv >= 0);
+	assert(target_size == 4);
+	assert(memcmp(output, "abcd", 4) == 0);
+}
+
+static void
+decompress_lz4(void) {
+	uint8_t input[] = {0x04, 0x22, 0x4d, 0x18, 0x68, 0x40, 0x04, 0x00, 0x00,
+					   0x00, 0x00, 0x00, 0x00, 0x00, 0xcd, 0x04, 0x00, 0x00,
+					   0x80, 0x61, 0x62, 0x63, 0x64, 0x00, 0x00, 0x00, 0x00};
+	size_t target_size = 4;
+	uint8_t output[4];
+
+	int rv = sqsh_extract_lz4(output, &target_size, input, sizeof(input));
+	assert(rv >= 0);
+	assert(target_size == 4);
+	assert(memcmp(output, "abcd", 4) == 0);
+}
+
+static void
+decompress_lzo2(void) {
+	uint8_t input[] = {
+			ZLIB_ABCD,
+	};
+	size_t target_size = 4;
+	uint8_t output[4];
+
+	int rv = sqsh_extract_lzo2(output, &target_size, input, sizeof(input));
+	assert(rv >= 0);
+	assert(target_size == 4);
+	assert(memcmp(output, "abcd", 4) == 0);
+}
+
+static void
+decompress_zlib(void) {
+	uint8_t input[] = {
+			ZLIB_ABCD,
+	};
+	size_t target_size = 4;
+	uint8_t output[4];
+
+	int rv = sqsh_extract_zlib(output, &target_size, input, sizeof(input));
+	assert(rv >= 0);
+	assert(target_size == 4);
+	assert(memcmp(output, "abcd", 4) == 0);
+}
+
+static void
+decompress_zstd(void) {
+	uint8_t input[] = {0x28, 0xb5, 0x2f, 0xfd, 0x20, 0x04, 0x21,
+					   0x00, 0x00, 0x61, 0x62, 0x63, 0x64};
+	size_t target_size = 4;
+	uint8_t output[4];
+
+	int rv = sqsh_extract_zstd(output, &target_size, input, sizeof(input));
+	assert(rv >= 0);
+	assert(target_size == 4);
+	assert(memcmp(output, "abcd", 4) == 0);
+}
+
+DEFINE
+TEST_OFF(decompress_lzma);
+TEST(decompress_xz);
+TEST_OFF(decompress_lz4);
+TEST_OFF(decompress_lzo2);
+TEST(decompress_zlib);
+TEST(decompress_zstd);
+DEFINE_END
