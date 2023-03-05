c63ac60c3230221cffdd2fc0753a4047ae9f0def
Enno Boland <g@s01.de>
2023-03-05 11:10:59 +0100

    test: make selftest run from meson

--- test/selftest.sh
+++ test/selftest.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-BUILD_DIR=build_dir
 set -ex
-cd "$(dirname "$0")"
-until [ -d .git ]; do cd ..; done
+
+: "${SQSH_LS:?SQSH_LS not set}"
+: "${SQSH_CAT:?SQSH_CAT not set}"
+: "${SOURCE_ROOT:?SOURCE_ROOT not set}"
+
+cd "$SOURCE_ROOT"
 
 tmpdir=$(mktemp -d)
 
@@ -13,15 +16,17 @@ UNPACKED="$tmpdir/squashfs-unpacked.img"
 mksquashfs .git "$PACKED" -noappend -keep-as-directory
 mksquashfs .git "$UNPACKED" -noappend -keep-as-directory -noI -noId -noD -noF -noX
 
-$BUILD_DIR/sqsh-ls -r "$PACKED" . > "$tmpdir"/packed.list
-$BUILD_DIR/sqsh-ls -r "$UNPACKED" . > "$tmpdir"/unpacked.list
+$SQSH_LS -r "$PACKED" . > "$tmpdir"/packed.list
+$SQSH_LS -r "$UNPACKED" . > "$tmpdir"/unpacked.list
 
 cmp "$tmpdir"/packed.list "$tmpdir"/unpacked.list
 
 while read -r; do
-	[ -f "$REPLY" ] && ! [ -h "$REPLY" ] || continue;
-	$BUILD_DIR/sqsh-cat "$PACKED" "$REPLY" | cmp - "$REPLY"
-	$BUILD_DIR/sqsh-cat "$UNPACKED" "$REPLY" | cmp - "$REPLY"
+	if ! [ -f "$REPLY" ] || [ -h "$REPLY" ]; then
+		continue;
+	fi
+	$SQSH_CAT "$PACKED" "$REPLY" | cmp - "$REPLY"
+	$SQSH_CAT "$UNPACKED" "$REPLY" | cmp - "$REPLY"
 done < "$tmpdir"/packed.list
 
 rm -r "$tmpdir"
