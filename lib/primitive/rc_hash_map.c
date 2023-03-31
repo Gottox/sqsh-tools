836bfd6403d7fb26b034476c8441d50816285701
Enno Boland <g@s01.de>
2023-03-31 07:56:03 +0200

    rc_hash_map: fix insertion to the wrong index

--- lib/primitive/rc_hash_map.c
+++ lib/primitive/rc_hash_map.c
@@ -115,8 +115,8 @@ sqsh__rc_hash_map_put(
 	struct SqshRcMap *values = NULL;
 	sqsh_rc_map_key_t *keys = NULL;
 
-	for (sqsh_index_t i = 0; i < size && i < MAX_COLLISIONS; i++) {
-		index = (index + 1) % size;
+	for (sqsh_index_t i = 0; i < size && i < MAX_COLLISIONS; i++, index++) {
+		index = index % size;
 
 		for (sqsh_index_t j = 0; j < hash_map->hash_map_count; j++) {
 			values = &hash_map->hash_maps[j].values;
@@ -152,8 +152,8 @@ sqsh__rc_hash_map_retain(
 	const size_t size = sqsh__rc_map_size(&hash_map->hash_maps[0].values);
 	sqsh_index_t index = key_to_index(key, size);
 
-	for (sqsh_index_t i = 0; i < size && i < MAX_COLLISIONS; i++) {
-		index = (index + 1) % size;
+	for (sqsh_index_t i = 0; i < size && i < MAX_COLLISIONS; i++, index++) {
+		index = index % size;
 
 		for (sqsh_index_t j = 0; j < hash_map->hash_map_count; j++) {
 			struct SqshRcMap *values = &hash_map->hash_maps[j].values;
