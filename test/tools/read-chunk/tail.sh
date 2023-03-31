2715c3294a09187a4e3f3a4b6d7557ec0495f8d8
Enno Boland <g@s01.de>
2023-03-31 17:22:12 +0200

    test: add read-chunk tail test

---
+++ test/tools/read-chunk/tail.sh
@@ -0,0 +1,38 @@
+#!/bin/sh -ex
+
+######################################################################
+# @author      : Enno Boland (mail@eboland.de)
+# @file        : tail.sh
+# @created     : Friday Mar 17, 2023 15:11:09 CET
+#
+# @description : This script creates a squashfs image, mounts it, and
+#                repacks it from the mounted path.
+######################################################################
+
+: "${BUILD_DIR:?BUILD_DIR is not set}"
+: "${MKSQUASHFS:?MKSQUASHFS is not set}"
+: "${SOURCE_ROOT:?SOURCE_ROOT is not set}"
+: "${SQSH_CAT:?SQSH_CAT is not set}"
+: "${READ_CHUNK:?READ_CHUNK is not set}"
+
+MKSQUASHFS_OPTS="-no-xattrs -noappend -mkfs-time 0 -b 4096"
+
+WORK_DIR="$BUILD_DIR/read-chunk-tail"
+
+mkdir -p "$WORK_DIR"
+cd "$WORK_DIR"
+
+OFFSET=262150
+
+seq 1 "$OFFSET" | head -c "$OFFSET" > "$PWD/file.orig"
+
+# shellcheck disable=SC2086
+$MKSQUASHFS "$PWD/file" "$PWD/original.squashfs" $MKSQUASHFS_OPTS
+
+mkdir -p "$PWD/mnt"
+
+$SQSH_CAT "$PWD/original.squashfs" file | tail -c 6 > tail.sqsh-cat
+$READ_CHUNK "$PWD/original.squashfs" file 262144 6 > tail.read-chunk
+
+
+exec cmp "$PWD/tail.sqsh-cat" "$PWD/tail.read-chunk"
