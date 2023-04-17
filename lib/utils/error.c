c6a7cd7c19d2fbeeef9f3d3afc3192972aec77ce
Enno Boland <g@s01.de>
2023-04-17 10:14:52 +0200

    error: cleanup unused error codes

--- lib/utils/error.c
+++ lib/utils/error.c
@@ -74,58 +74,22 @@ sqsh_error_str(int error_code) {
 		return "Blocksize missmatch";
 	case SQSH_ERROR_SIZE_MISSMATCH:
 		return "Size missmatch";
-	case SQSH_ERROR_CHECKFLAG_SET:
-		return "Checkflag set";
-	case SQSH_ERROR_METABLOCK_UNSUPPORTED_COMPRESSION:
-		return "Metablock unsupported compression";
-	case SQSH_ERROR_METABLOCK_INFO_IS_COMPRESSED:
-		return "Metablock info is compressed";
-	case SQSH_ERROR_METABLOCK_INIT:
-		return "Metablock init";
 	case SQSH_ERROR_COMPRESSION_INIT:
 		return "Compression init";
 	case SQSH_ERROR_COMPRESSION_DECOMPRESS:
 		return "Compression decompress";
 	case SQSH_ERROR_UNKOWN_INODE_TYPE:
 		return "Unkown inode type";
-	case SQSH_ERROR_COMPRESSION_STREAM_INIT:
-		return "Compression stream init";
-	case SQSH_ERROR_COMPRESSION_STREAM_CLEANUP:
-		return "Compression stream cleanup";
-	case SQSH_ERROR_STREAM_NOT_ENOUGH_BYTES:
-		return "Stream not enough bytes";
-	case SQSH_ERROR_GZIP_HEADER_TOO_SMALL:
-		return "Gzip header too small";
-	case SQSH_ERROR_GZIP_HEADER_READ:
-		return "Gzip header read";
-	case SQSH_ERROR_GZIP_READ_AFTER_END:
-		return "Gzip read after end";
 	case SQSH_ERROR_NOT_A_DIRECTORY:
 		return "Not a directory";
 	case SQSH_ERROR_NOT_A_FILE:
 		return "Not a file";
 	case SQSH_ERROR_MALLOC_FAILED:
 		return "Malloc Failed";
-	case SQSH_ERROR_DIRECTORY_INIT:
-		return "Directory Init";
-	case SQSH_ERROR_INODE_INIT:
-		return "Inode init";
 	case SQSH_ERROR_INTEGER_OVERFLOW:
 		return "Integer overflow";
 	case SQSH_ERROR_NO_SUCH_FILE:
 		return "No such file or directory";
-	case SQSH_ERROR_METABLOCK_ZERO_SIZE:
-		return "Metablock with size zero";
-	case SQSH_ERROR_SEEK_OUT_OF_RANGE:
-		return "Seek out of range";
-	case SQSH_ERROR_SEEK_IN_FRAGMENT:
-		return "Seek in fragment";
-	case SQSH_ERROR_NO_FRAGMENT:
-		return "No fragment";
-	case SQSH_ERROR_NO_DATABLOCKS:
-		return "No datablocks";
-	case SQSH_ERROR_HASHMAP_INTERNAL_ERROR:
-		return "Hashmap internal error";
 	case SQSH_ERROR_NO_EXTENDED_DIRECTORY:
 		return "No extended directory";
 	case SQSH_ERROR_NO_FRAGMENT_TABLE:
@@ -134,16 +98,12 @@ sqsh_error_str(int error_code) {
 		return "No export table";
 	case SQSH_ERROR_NO_XATTR_TABLE:
 		return "No xattr table";
-	case SQSH_ERROR_METABLOCK_TOO_BIG:
-		return "Metablock too big";
 	case SQSH_ERROR_MAPPER_INIT:
 		return "Mapper init error";
 	case SQSH_ERROR_MAPPER_MAP:
 		return "Mapper mapping error";
 	case SQSH_ERROR_COMPRESSION_UNSUPPORTED:
 		return "Compression unkown";
-	case SQSH_ERROR_INDEX_OUT_OF_BOUNDS:
-		return "Index out of bounds";
 	case SQSH_ERROR_CURL_INVALID_RANGE_HEADER:
 		return "Invalid range header";
 	case SQSH_ERROR_TODO:
