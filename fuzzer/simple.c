f92785820d1e243b93d8724ed8460c8a34d907d5
Enno Boland <g@s01.de>
2023-05-15 11:26:38 +0200

    fuzzer: integrate in meson

--- fuzzer/simple.c
+++ fuzzer/simple.c
@@ -6,69 +6,79 @@
  */
 
 #include "../include/sqsh_archive_private.h"
-#include "../include/sqsh_context.h"
 #include "../include/sqsh_directory_private.h"
 #include "../include/sqsh_file_private.h"
 #include "../include/sqsh_inode_private.h"
 #include <stdint.h>
 
 static int
-read_file(struct SqshInode *inode) {
-	struct SqshFileContext file = {0};
+read_file(struct SqshDirectoryIterator *iter) {
+	struct SqshInode *inode = NULL;
+	struct SqshFileReader file = {0};
 	int rv;
-	rv = sqsh__file_init(&file, inode);
+
+	inode = sqsh_directory_iterator_inode_load(iter, &rv);
 	if (rv < 0) {
 		goto out;
 	}
 
-	rv = sqsh_file_read(&file, sqsh_inode_file_size(inode));
+	rv = sqsh__file_reader_init(&file, inode);
+	if (rv < 0) {
+		goto out;
+	}
+
+	size_t file_size = sqsh_inode_file_size(inode);
+	rv = sqsh_file_reader_advance(&file, 0, file_size);
 	if (rv < 0) {
 		goto out;
 	}
 
 out:
-	sqsh__file_cleanup(&file);
-	// noop
+	sqsh_inode_free(inode);
+	sqsh__file_reader_cleanup(&file);
 	return 0;
 }
+
 int
 LLVMFuzzerTestOneInput(char *data, size_t size) {
 	int rv = 0;
-	struct SqshArchive sqsh = {0};
-	struct SqshInode inode = {0};
-	struct SqshDirectoryIterator iter = {0};
-	const struct SqshSuperblock *superblock;
-	rv = sqsh__archive_init(
-			&sqsh, (uint8_t *)data,
+	struct SqshArchive *archive = NULL;
+	struct SqshInode *inode = NULL;
+	struct SqshDirectoryIterator *iter = NULL;
+	const struct SqshSuperblock *superblock = NULL;
+
+	archive = sqsh_archive_new(
+			(uint8_t *)data,
 			&(struct SqshConfig){
 					.source_mapper = sqsh_mapper_impl_static,
 					.source_size = size,
-			});
+			},
+			&rv);
 	if (rv < 0) {
 		goto out;
 	}
 
-	superblock = sqsh_archive_superblock(&sqsh);
+	superblock = sqsh_archive_superblock(archive);
 	uint64_t inode_ref = sqsh_superblock_inode_root_ref(superblock);
-	rv = sqsh__inode_init(&inode, &sqsh, inode_ref);
+	inode = sqsh_inode_new(archive, inode_ref, &rv);
 	if (rv < 0) {
 		goto out;
 	}
 
-	rv = sqsh__directory_iterator_init(&iter, &inode);
-	if (rv < 0) {
+	iter = sqsh_directory_iterator_new(inode, &rv);
+	if (iter == NULL) {
 		goto out;
 	}
-	while (sqsh_directory_iterator_next(&iter)) {
-		if (read_file(&inode) < 0)
+	while (sqsh_directory_iterator_next(iter)) {
+		if (read_file(iter) < 0)
 			break;
 	}
 
 out:
 
-	sqsh__directory_iterator_cleanup(&iter);
-	sqsh__inode_cleanup(&inode);
-	sqsh__archive_cleanup(&sqsh);
+	sqsh_directory_iterator_free(iter);
+	sqsh_inode_free(inode);
+	sqsh_archive_free(archive);
 
 	return rv; // Non-zero return values are reserved for future use.
 }
