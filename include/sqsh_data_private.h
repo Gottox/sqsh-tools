596e4417149add3162ce25941018a1386643cede
Enno Boland <g@s01.de>
2023-09-07 07:42:17 +0200

    data: remove size macros by sizeof() for compression options

--- include/sqsh_data_private.h
+++ include/sqsh_data_private.h
@@ -41,52 +41,30 @@ extern "C" {
  * data/compression_options_data.c
  */
 
-#define SQSH_SIZEOF_COMPRESSION_OPTIONS_GZIP 8
-#define SQSH_SIZEOF_COMPRESSION_OPTIONS_XZ 8
-#define SQSH_SIZEOF_COMPRESSION_OPTIONS_LZ4 8
-#define SQSH_SIZEOF_COMPRESSION_OPTIONS_ZSTD 4
-#define SQSH_SIZEOF_COMPRESSION_OPTIONS_LZO 8
-#define SQSH_SIZEOF_COMPRESSION_OPTIONS 8
-
 struct SQSH_UNALIGNED SqshDataCompressionOptionsGzip {
 	uint32_t compression_level;
 	uint16_t window_size;
 	uint16_t strategies;
 };
-SQSH_STATIC_ASSERT(
-		sizeof(struct SqshDataCompressionOptionsGzip) ==
-		SQSH_SIZEOF_COMPRESSION_OPTIONS_GZIP);
 
 struct SQSH_UNALIGNED SqshDataCompressionOptionsXz {
 	uint32_t dictionary_size;
 	uint32_t filters;
 };
-SQSH_STATIC_ASSERT(
-		sizeof(struct SqshDataCompressionOptionsXz) ==
-		SQSH_SIZEOF_COMPRESSION_OPTIONS_XZ);
 
 struct SQSH_UNALIGNED SqshDataCompressionOptionsLz4 {
 	uint32_t version;
 	uint32_t flags;
 };
-SQSH_STATIC_ASSERT(
-		sizeof(struct SqshDataCompressionOptionsLz4) ==
-		SQSH_SIZEOF_COMPRESSION_OPTIONS_LZ4);
 
 struct SQSH_UNALIGNED SqshDataCompressionOptionsZstd {
 	uint32_t compression_level;
 };
-SQSH_STATIC_ASSERT(
-		sizeof(struct SqshDataCompressionOptionsZstd) ==
-		SQSH_SIZEOF_COMPRESSION_OPTIONS_ZSTD);
 
 struct SQSH_UNALIGNED SqshDataCompressionOptionsLzo {
 	uint32_t algorithm;
 	uint32_t compression_level;
 };
-SQSH_STATIC_ASSERT(
-		sizeof(struct SqshDataCompressionOptionsLzo) ==
-		SQSH_SIZEOF_COMPRESSION_OPTIONS_LZO);
 
 union SqshDataCompressionOptions {
 	struct SqshDataCompressionOptionsGzip gzip;
@@ -95,9 +73,6 @@ union SqshDataCompressionOptions {
 	struct SqshDataCompressionOptionsZstd zstd;
 	struct SqshDataCompressionOptionsLzo lzo;
 };
-SQSH_STATIC_ASSERT(
-		sizeof(union SqshDataCompressionOptions) ==
-		SQSH_SIZEOF_COMPRESSION_OPTIONS);
 
 SQSH_NO_EXPORT uint32_t sqsh_compression_data_options_gzip_compression_level(
 		const union SqshDataCompressionOptions *options);
