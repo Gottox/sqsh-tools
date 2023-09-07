be2368b11111f7a3607931a0ad6b6ea2a81d4347
Enno Boland <g@s01.de>
2023-09-07 07:37:09 +0200

    data: move compression options definitions to headers

--- lib/data/compression_options_data.c
+++ lib/data/compression_options_data.c
@@ -37,57 +37,6 @@
 
 #include <cextras/endian_compat.h>
 
-struct SQSH_UNALIGNED SqshDataCompressionOptionsGzip {
-	uint32_t compression_level;
-	uint16_t window_size;
-	uint16_t strategies;
-};
-SQSH_STATIC_ASSERT(
-		sizeof(struct SqshDataCompressionOptionsGzip) ==
-		SQSH_SIZEOF_COMPRESSION_OPTIONS_GZIP);
-
-struct SQSH_UNALIGNED SqshDataCompressionOptionsXz {
-	uint32_t dictionary_size;
-	uint32_t filters;
-};
-SQSH_STATIC_ASSERT(
-		sizeof(struct SqshDataCompressionOptionsXz) ==
-		SQSH_SIZEOF_COMPRESSION_OPTIONS_XZ);
-
-struct SQSH_UNALIGNED SqshDataCompressionOptionsLz4 {
-	uint32_t version;
-	uint32_t flags;
-};
-SQSH_STATIC_ASSERT(
-		sizeof(struct SqshDataCompressionOptionsLz4) ==
-		SQSH_SIZEOF_COMPRESSION_OPTIONS_LZ4);
-
-struct SQSH_UNALIGNED SqshDataCompressionOptionsZstd {
-	uint32_t compression_level;
-};
-SQSH_STATIC_ASSERT(
-		sizeof(struct SqshDataCompressionOptionsZstd) ==
-		SQSH_SIZEOF_COMPRESSION_OPTIONS_ZSTD);
-
-struct SQSH_UNALIGNED SqshDataCompressionOptionsLzo {
-	uint32_t algorithm;
-	uint32_t compression_level;
-};
-SQSH_STATIC_ASSERT(
-		sizeof(struct SqshDataCompressionOptionsLzo) ==
-		SQSH_SIZEOF_COMPRESSION_OPTIONS_LZO);
-
-union SqshDataCompressionOptions {
-	struct SqshDataCompressionOptionsGzip gzip;
-	struct SqshDataCompressionOptionsXz xz;
-	struct SqshDataCompressionOptionsLz4 lz4;
-	struct SqshDataCompressionOptionsZstd zstd;
-	struct SqshDataCompressionOptionsLzo lzo;
-};
-SQSH_STATIC_ASSERT(
-		sizeof(union SqshDataCompressionOptions) ==
-		SQSH_SIZEOF_COMPRESSION_OPTIONS);
-
 uint32_t
 sqsh_compression_data_options_gzip_compression_level(
 		const union SqshDataCompressionOptions *options) {
