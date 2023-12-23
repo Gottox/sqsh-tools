20637cc86361db0bb24d6886dd0d7a2d5e08c8a4
Enno Boland <g@s01.de>
2023-12-23 07:42:18 +0100

    common: add SQSH_UNLIKELY macro

--- common/include/sqsh_common_private.h
+++ common/include/sqsh_common_private.h
@@ -47,16 +47,18 @@ extern "C" {
 #define SQSH_MIN(a, b) ((a) < (b) ? (a) : (b))
 #define SQSH_MAX(a, b) ((a) > (b) ? (a) : (b))
 
+#define SQSH_UNLIKELY(x) __builtin_expect(!!(x), 0)
+
 #define SQSH__ADD_OVERFLOW(a, b, res) __builtin_add_overflow(a, b, res)
 #define SQSH__SUB_OVERFLOW(a, b, res) __builtin_sub_overflow(a, b, res)
 #define SQSH__MULT_OVERFLOW(a, b, res) __builtin_mul_overflow(a, b, res)
 
 #define SQSH_ADD_OVERFLOW(a, b, res) \
-	__builtin_expect(!!SQSH__ADD_OVERFLOW(a, b, res), 0)
+	SQSH_UNLIKELY(SQSH__ADD_OVERFLOW(a, b, res))
 #define SQSH_SUB_OVERFLOW(a, b, res) \
-	__builtin_expect(!!SQSH__SUB_OVERFLOW(a, b, res), 0)
+	SQSH_UNLIKELY(SQSH__SUB_OVERFLOW(a, b, res))
 #define SQSH_MULT_OVERFLOW(a, b, res) \
-	__builtin_expect(!!SQSH__MULT_OVERFLOW(a, b, res), 0)
+	SQSH_UNLIKELY(SQSH__MULT_OVERFLOW(a, b, res))
 
 #define SQSH_DIVIDE_CEIL(x, y) ((x) / (y) + !!((x) % (y)))
 #define SQSH_PADDING(x, p) (SQSH_DIVIDE_CEIL(x, p) * p)
