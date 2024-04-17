1e837cd4514345d8c23c65cc65a60d408d7d4ce7
Enno Boland <g@s01.de>
2024-04-17 22:40:28 +0200

    WIP

--- libsqsh/src/extract/radix.c
+++ libsqsh/src/extract/radix.c
@@ -1,12 +1,14 @@
-#include <stdlib.h>
 #include <sqsh_extract_private.h>
+#include <stdlib.h>
 
-int sqsh__extract_radix_init(struct SqshExtractRadix *radix) {
+int
+sqsh__extract_radix_init(struct SqshExtractRadix *radix) {
 	(void)radix;
 	return 0;
 }
 
-int sqsh__extract_radix_cleanup(struct SqshExtractRadix *radix) {
+int
+sqsh__extract_radix_cleanup(struct SqshExtractRadix *radix) {
 	(void)radix;
 	return 0;
 }
