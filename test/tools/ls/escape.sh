4e36771941942223164770cb3a5fa6d1fce7652b
Enno Boland <g@s01.de>
2024-06-04 09:32:37 +0200

    test: test escaping of outputs

---
+++ test/tools/ls/escape.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -ex
+
+######################################################################
+# @author       Enno Boland (mail@eboland.de)
+# @file         escape.sh
+# @created      Monday Nov 20, 2023 18:24:05 CET
+
+#
+# @description  This script creates a squashfs image, mounts it, and
+#               repacks it from the mounted path.
+######################################################################
+
+: "${BUILD_DIR:?BUILD_DIR is not set}"
+: "${MKSQUASHFS:?MKSQUASHFS is not set}"
+: "${SOURCE_ROOT:?SOURCE_ROOT is not set}"
+: "${SQSH_LS:?SQSH_UNPACK is not set}"
+
+MKSQUASHFS_OPTS="-no-xattrs -noappend -all-root -mkfs-time 0"
+
+WORK_DIR="$BUILD_DIR/escape"
+
+mkdir -p "$WORK_DIR"
+cd "$WORK_DIR"
+
+mkdir -p "$PWD/empty"
+
+printf "dir\x1b d 777 0 0\n" > "$PWD/escape.pseudo";
+
+# shellcheck disable=SC2086
+$MKSQUASHFS "$PWD/empty" "$PWD/escape.squashfs" -pf "$PWD/escape.pseudo" \
+	$MKSQUASHFS_OPTS
+result=$($SQSH_LS -r --escape "$PWD/escape.squashfs")
+[ "$result" = "/dir\e" ]
+
+result=$($SQSH_LS -r --raw "$PWD/escape.squashfs")
+[ "$result" = "$(printf "/dir\e")" ]
