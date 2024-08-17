face878431fc2c4e1bc433dbcdfefef738d8add7
Enno Boland <g@s01.de>
2024-08-17 10:59:26 +0200

    archive: Add a test to ensure backwards compatibility
    This commit adds a unit test to ensure that the struct SqshConfig
    is not larger than the last backwards compatible version.

--- test/libsqsh/archive/archive.c
+++ test/libsqsh/archive/archive.c
@@ -40,127 +40,46 @@
 #include <sqsh_data_private.h>
 #include <sqsh_file_private.h>
 
-UTEST(file, load_file) {
-	int rv;
-	struct SqshArchive archive = {0};
-	struct SqshFile file = {0};
-	uint8_t payload[8192] = {
-			SQSH_HEADER,
-			/* inode */
-			[INODE_TABLE_OFFSET + 15] = METABLOCK_HEADER(0, 128),
-			0,
-			0,
-			0,
-			INODE_HEADER(2, 0666, 0, 0, 4242, 1),
-			INODE_BASIC_FILE(1024, 0xFFFFFFFF, 0, 1),
-			UINT32_BYTES(42),
+// The SqshConfig struct as it was in version 1.0. This is used to perform ABI
+// compatibility checks.
 
-	};
-	mk_stub(&archive, payload, sizeof(payload));
+struct SqshConfigV1_0 {
+	uint64_t archive_offset;
+	uint64_t source_size;
+	const struct SqshMemoryMapperImpl *source_mapper;
+	int mapper_block_size;
+	int mapper_lru_size;
+	int compression_lru_size;
+	size_t max_symlink_depth;
+	char _reserved[128];
+};
 
-	uint64_t inode_ref = sqsh_address_ref_create(15, 3);
-	rv = sqsh__file_init(&file, &archive, inode_ref);
-	ASSERT_EQ(0, rv);
+UTEST(config, config_compat_check_v1_0) {
+	ASSERT_EQ(
+			offsetof(struct SqshConfig, archive_offset),
+			offsetof(struct SqshConfigV1_0, archive_offset));
+	ASSERT_EQ(
+			offsetof(struct SqshConfig, source_size),
+			offsetof(struct SqshConfigV1_0, source_size));
+	ASSERT_EQ(
+			offsetof(struct SqshConfig, source_mapper),
+			offsetof(struct SqshConfigV1_0, source_mapper));
+	ASSERT_EQ(
+			offsetof(struct SqshConfig, mapper_block_size),
+			offsetof(struct SqshConfigV1_0, mapper_block_size));
+	ASSERT_EQ(
+			offsetof(struct SqshConfig, mapper_lru_size),
+			offsetof(struct SqshConfigV1_0, mapper_lru_size));
+	ASSERT_EQ(
+			offsetof(struct SqshConfig, compression_lru_size),
+			offsetof(struct SqshConfigV1_0, compression_lru_size));
+	ASSERT_EQ(
+			offsetof(struct SqshConfig, max_symlink_depth),
+			offsetof(struct SqshConfigV1_0, max_symlink_depth));
 
-	ASSERT_EQ(SQSH_FILE_TYPE_FILE, sqsh_file_type(&file));
-	ASSERT_EQ(0666, sqsh_file_permission(&file));
-	ASSERT_EQ((uint32_t)4242, sqsh_file_modified_time(&file));
-	ASSERT_EQ((uint32_t)1024, sqsh_file_blocks_start(&file));
-	ASSERT_EQ(false, sqsh_file_has_fragment(&file));
-
-	ASSERT_EQ((uint32_t)1, sqsh_file_block_count(&file));
-	ASSERT_EQ((uint32_t)42, sqsh_file_block_size(&file, 0));
-
-	sqsh__file_cleanup(&file);
-	sqsh__archive_cleanup(&archive);
-}
-
-UTEST(file, resolve_file) {
-	int rv = 0;
-	struct SqshArchive archive = {0};
-	uint8_t payload[] = {
-			/* clang-format off */
-			SQSH_HEADER,
-			[1024] = '1', '2', '3', '4', '5', '6', '7', '8',
-			/* inode */
-			[INODE_TABLE_OFFSET] = METABLOCK_HEADER(0, 1024),
-			INODE_HEADER(1, 0, 0, 0, 0, 1),
-			INODE_BASIC_DIR(0, 1024, 0, 0),
-			[INODE_TABLE_OFFSET+2+128] =
-			INODE_HEADER(3, 0, 0, 0, 0, 2),
-			INODE_BASIC_SYMLINK(3),
-			't', 'g', 't',
-			[INODE_TABLE_OFFSET+2+256] =
-			INODE_HEADER(2, 0, 0, 0, 0, 3),
-			INODE_BASIC_FILE(1024, 0xFFFFFFFF, 0, 8),
-			DATA_BLOCK_REF(8, 0),
-			[DIRECTORY_TABLE_OFFSET] = METABLOCK_HEADER(0, 128),
-			DIRECTORY_HEADER(2, 0, 0),
-			DIRECTORY_ENTRY(128, 2, 3, 3),
-			's', 'r', 'c',
-			DIRECTORY_ENTRY(256, 3, 2, 3),
-			't', 'g', 't',
-			[FRAGMENT_TABLE_OFFSET] = 0,
-			/* clang-format on */
-	};
-	mk_stub(&archive, payload, sizeof(payload));
-
-	struct SqshFile *symlink = sqsh_lopen(&archive, "/src", &rv);
-	ASSERT_EQ(0, rv);
-	ASSERT_EQ(SQSH_FILE_TYPE_SYMLINK, sqsh_file_type(symlink));
-
-	rv = sqsh_file_symlink_resolve(symlink);
-	ASSERT_EQ(0, rv);
-	ASSERT_EQ(SQSH_FILE_TYPE_FILE, sqsh_file_type(symlink));
-	ASSERT_EQ((uint32_t)3, sqsh_file_inode(symlink));
-
-	sqsh_close(symlink);
-
-	sqsh__archive_cleanup(&archive);
-}
-
-UTEST(file, resolve_unkown_dir_inode) {
-	int rv = 0;
-	struct SqshArchive archive = {0};
-	uint8_t payload[] = {
-			/* clang-format off */
-			SQSH_HEADER,
-			[1024] = '1', '2', '3', '4', '5', '6', '7', '8',
-			/* inode */
-			[INODE_TABLE_OFFSET] = METABLOCK_HEADER(0, 1024),
-			INODE_HEADER(1, 0, 0, 0, 0, 1),
-			INODE_BASIC_DIR(0, 1024, 0, 0),
-			[INODE_TABLE_OFFSET+2+128] =
-			INODE_HEADER(3, 0, 0, 0, 0, 2),
-			INODE_BASIC_SYMLINK(3),
-			't', 'g', 't',
-			[INODE_TABLE_OFFSET+2+256] =
-			INODE_HEADER(2, 0, 0, 0, 0, 3),
-			INODE_BASIC_FILE(1024, 0xFFFFFFFF, 0, 8),
-			DATA_BLOCK_REF(8, 0),
-			[DIRECTORY_TABLE_OFFSET] = METABLOCK_HEADER(0, 128),
-			DIRECTORY_HEADER(2, 0, 0),
-			DIRECTORY_ENTRY(128, 2, 3, 3),
-			's', 'r', 'c',
-			DIRECTORY_ENTRY(256, 3, 2, 3),
-			't', 'g', 't',
-			[FRAGMENT_TABLE_OFFSET] = 0,
-			/* clang-format on */
-	};
-	mk_stub(&archive, payload, sizeof(payload));
-
-	struct SqshFile *symlink = sqsh_lopen(&archive, "/src", &rv);
-	ASSERT_EQ(0, rv);
-	ASSERT_EQ(SQSH_FILE_TYPE_SYMLINK, sqsh_file_type(symlink));
-	symlink->dir_inode = 0;
-	symlink->has_dir_inode = false;
-
-	rv = sqsh_file_symlink_resolve(symlink);
-	ASSERT_EQ(-SQSH_ERROR_INODE_PARENT_UNSET, rv);
-
-	sqsh_close(symlink);
-
-	sqsh__archive_cleanup(&archive);
+	ASSERT_LT(
+			offsetof(struct SqshConfig, _reserved),
+			sizeof(struct SqshConfigV1_0));
 }
 
 UTEST_MAIN()
