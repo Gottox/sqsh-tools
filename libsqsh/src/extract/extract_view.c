23f32a5cdabddc4b8b4a84baddebcec5d5267d76
Enno Boland <g@s01.de>
2026-05-15 16:43:14 +0200

    extract_view: make extract view more robust against race conditions

--- libsqsh/src/extract/extract_view.c
+++ libsqsh/src/extract/extract_view.c
@@ -41,17 +41,17 @@ sqsh__extract_view_init(
 		struct SqshExtractView *view, struct SqshExtractManager *manager,
 		const struct SqshMapReader *reader) {
 	int rv = 0;
-	view->manager = manager;
-	view->buffer = NULL;
-	view->address = sqsh__map_reader_address(reader);
+	memset(view, 0, sizeof(struct SqshExtractView));
+
+	const uint64_t address = sqsh__map_reader_address(reader);
 
 	rv = sqsh__extract_manager_uncompress(manager, reader, &view->buffer);
 	if (rv < 0) {
 		goto out;
 	}
-
+	view->address = address;
+	view->manager = manager;
 	view->size = cx_buffer_size(view->buffer);
-
 out:
 	if (rv < 0) {
 		sqsh__extract_view_cleanup(view);
@@ -63,8 +63,8 @@ int
 sqsh__extract_view_copy(
 		struct SqshExtractView *target, const struct SqshExtractView *source) {
 	int rv = 0;
+	memset(target, 0, sizeof(*target)); /* mirror init's pattern */
 
-	target->manager = source->manager;
 	if (source->buffer) {
 		rv = sqsh__extract_manager_retain_buffer(
 				source->manager, source->buffer);
@@ -72,9 +72,10 @@ sqsh__extract_view_copy(
 			goto out;
 		}
 	}
+	target->address = source->address;
+	target->manager = source->manager;
 	target->buffer = source->buffer;
 	target->size = source->size;
-	target->address = source->address;
 out:
 	if (rv < 0) {
 		sqsh__extract_view_cleanup(target);
@@ -98,9 +99,7 @@ sqsh__extract_view_cleanup(struct SqshExtractView *view) {
 
 	if (view->manager != NULL) {
 		rv = sqsh__extract_manager_release(view->manager, view->address);
-		view->manager = NULL;
 	}
-	view->buffer = NULL;
-	view->size = 0;
+	memset(view, 0, sizeof(struct SqshExtractView));
 	return rv;
 }
